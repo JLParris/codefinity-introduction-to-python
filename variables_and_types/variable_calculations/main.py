874972b51dec5ce4ffaefa6389c7855a2d1ce62e
JLParris <>
2026-02-15 23:37:36 +0000

    Variable calculations

--- variables_and_types/variable_calculations/main.py
+++ variables_and_types/variable_calculations/main.py
@@ -5,4 +5,4 @@ total_cost = (purchase_quantity*item_price)
 
 
 # Testing
-print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $",total_cost)
